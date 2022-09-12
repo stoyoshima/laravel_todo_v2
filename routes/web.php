1ed4551075466124350c0786440bb81ff23f7c96
toyoshima <sxtsham9129@yahoo.ne.jp>
2022-09-13 08:56:18 +0900

    todoリスト完成

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,6 @@ use App\Http\Controllers\TaskController;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/',[TaskController::class,'index']);
 
 Route::resource('tasks', TaskController::class, ['only' => ['index', 'store', 'show', 'edit', 'update', 'destroy']]);
